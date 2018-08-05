b42c24989f0b0e743d99d7c252539301c9e4009d
q-shivakumarjavali <shivakumar.javali@sprinklr.com>
2018-08-05 18:50:09 +0530

    Hello world program

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by shivjavali on 05/08/18.
+ */
+public class HelloWorld {
+
+    public final static int value = 10;
+
+    public static void main(String[] args) {
+        for (int i=0;i<value;i++){
+            System.out.println("Hello world : "+i);
+        }
+    }
+}
